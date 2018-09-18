e0bb317d059d1185fa4112866e57a3b72ef11440
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 19:51:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   end
-  if array.length == 2
+  if array.length == 3
     array.join(", ")
     array.insert(-2, "and")
   end
