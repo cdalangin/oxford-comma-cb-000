325c05b23501bf57733db23395c55351c255604c
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-18 20:13:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,6 @@ def oxford_comma(array)
     return array.join(" and ")
   end
   if array.length == 3
-    array.join(", ")
-    array.insert(-2, "and")
+    array.join(", ").insert(-2, "and")
   end
 end
